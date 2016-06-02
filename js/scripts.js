96d3d9e15c3044937645bf6382b26484c0b77b5d
J. Ryan Conklin and Stephen Emery <emery.stephen@gmail.com>
2016-06-01 18:48:29 -0700

    Fill out Javascript functions for calculator, add form for user interface in HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (Back-End) Logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -6,8 +8,26 @@ var subtract = function(number1, number2) {
   return number1 - number2;
 };
 
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = subtract(number1, number2);
-alert(result);
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User Interface (Front End) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+
+    alert(add(number1, number2));
+  });
+});
+
+
+// var result = divide(number1, number2);
+// alert(result);
